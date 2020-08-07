a9f16b191e4c713e78a296927532f66d9e4bac40
Paul <lanshunfang@gmail.com>
2020-08-07 13:29:40 -0700

    Remote unnecessary files

--- bin/hadoop-cmd.sh
+++
@@ -1,3 +0,0 @@
-hadoop fs -mkdir -p /machine-learning-final/output
-hadoop fs -put /Users/lanshunfang/Downloads/_kaggle_data/kaggle-black-friday/train.csv /machine-learning-final
-hadoop fs -head /machine-learning-final/train.csv
